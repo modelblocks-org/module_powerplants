75ddf460086b230cd2e456ff98d2de2347826666
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2025-06-29 15:01:29 +0200

    Separate GEM steps into individual categories

--- workflow/rules/gem.smk
+++ workflow/rules/gem.smk
@@ -1,19 +1,36 @@
-rule prepare_gem:
+rule prepare_gem_non_combustion:
     message:
-        "Global Energy Monitor: obtain combustion, nuclear and geothermal powerplants."
+        "Global Energy Monitor: obtain powerplants - {wildcards.category}."
     params:
-        fuel_mapping=internal["gem"]["fuel_mapping"],
-        default_fuel=config["categories"]["default_fuel"],
-        technology_mapping=config["categories"]["technology_mapping"]
+        technology_mapping = lambda wc: config["categories"]["technology_mapping"][wc.category]
     input:
         gem_raw="resources/automatic/gem/integrated.xlsx",
     output:
-        combustion_plants="resources/automatic/prepared/combustion.parquet",
-        combustion_plant_fuels="resources/automatic/prepared/combustion_fuels.parquet",
-        nuclear_plants="resources/automatic/prepared/nuclear.parquet",
-        geothermal_plants="resources/automatic/prepared/geothermal.parquet"
+        powerplant_capacity="resources/automatic/prepared/{category}.parquet",
+    wildcard_constraints:
+        category="|".join(["nuclear", "geothermal"])
     log:
-        "logs/prepare_gem.log",
+        "logs/prepare_gem_non_combustion_{category}.log",
+    conda:
+        "../envs/shapes.yaml"
+    script:
+        "../scripts/prepare_gem.py"
+
+rule prepare_gem_combustion:
+    message:
+        "Global Energy Monitor: obtain combustion powerplants - {wildcards.category}."
+    params:
+        default_fuel = lambda wc: config["categories"]["default_fuel"][wc.category],
+        technology_mapping = lambda wc: config["categories"]["technology_mapping"][wc.category]
+    input:
+        gem_raw="resources/automatic/gem/integrated.xlsx",
+    output:
+        powerplant_capacity="resources/automatic/prepared/{category}.parquet",
+        powerplant_fuels="resources/automatic/prepared/{category}_fuels.parquet"
+    wildcard_constraints:
+        category="|".join(["bioenergy", "coal", "oil_gas"])
+    log:
+        "logs/prepare_gem_combustion_{category}.log",
     conda:
         "../envs/shapes.yaml"
     script:
--- workflow/scripts/prepare_gem.py
+++ workflow/scripts/prepare_gem.py
@@ -2,52 +2,68 @@
 
 import re
 import sys
-from typing import TYPE_CHECKING, Any, Literal, TypedDict
+from typing import TYPE_CHECKING, Any, Literal
 
 import _schemas
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+from _utils import get_point
 
 if TYPE_CHECKING:
     snakemake: Any
 sys.stderr = open(snakemake.log[0], "w")
 
 
-class SmkInput(TypedDict):
-    """Snakemake inputs."""
+INVALID_STATE_VALUES = ["cancelled", "shelved"]
+DROP_NA_COLUMNS = ["Capacity (MW)", "Latitude", "Longitude"]
+CATEGORY_MAPPING = {"oil/gas": "oil_gas"}
+COMBUSTION_CATEGORIES = ["bioenergy", "coal", "oil_gas"]
 
-    gem_raw: str
-
-
-class SmkOutput(TypedDict):
-    """Snakemake outputs."""
-
-    combustion_plants: str
-    combustion_plant_fuels: str
-    geothermal_plants: str
-    nuclear_plants: str
-
-
-class CategoryParam(TypedDict):
-    """Powerplant categories."""
-
-    bioenergy: dict[str, str]
-    coal: dict[str, str]
-    oil_n_gas: dict[str, str]
-    nuclear: dict[str, str]
-    geothermal: dict[str, str]
-
-
-class SmkParams(TypedDict):
-    """Snakemake parameters."""
-
-    fuel_mapping: dict[str, str]
-    default_fuel: CategoryParam
-    technology_mapping: CategoryParam
-
-
-FUEL_MAPPING = snakemake.params["fuel_mapping"]
+FUEL_MAPPING = {
+    "fossil gas: natural gas": "natural gas",
+    "fossil gas: LNG": "natural gas",
+    "fossil gas: waste heat from natural gas": "waste heat",
+    "industrial by-product": "waste heat",
+    "fossil gas: coalbed methane": "natural gas",
+    "fossil liquids: crude oil": "oil",
+    "fossil liquids: diesel": "diesel",
+    "fossil liquids: fuel oil": "oil",
+    "fossil liquids: heavy fuel oil": "oil",
+    "fossil liquids: light fuel oil": "oil",
+    "fossil liquids: petroleum coke": "oil",
+    "fossil liquids: jet fuel": "oil",
+    "fossil liquids: liquefied petroleum gas": "oil",
+    "fossil liquids: naphtha": "oil",
+    "fossil liquids: gasoline": "gasoline",
+    "other: hydrogen (green)": "hydrogen",
+    "other: hydrogen (unknown)": "hydrogen",
+    "fossil liquids: waste/other oil": "oil",
+    "fossil liquids: kerosene": "oil",
+    "fossil gas: gaseous propane": "oil",
+    "bituminous": "hard coal",
+    "lignite": "brown coal",
+    "subbituminous": "brown coal",
+    "waste coal": "brown coal",
+    "anthracite": "hard coal",
+    "bioenergy: unknown": "biomass",
+    "bioenergy: refuse (municipal and industrial wastes)": "solid waste",
+    "bioenergy: paper mill wastes": "biomass",
+    "bioenergy: refuse (landfill gas)": "biogas",
+    "bioenergy: agricultural waste (biogas)": "biogas",
+    "bioenergy: wood & other biomass (solids)": "biomass",
+    "bioenergy: agricultural waste (solids)": "biomass",
+    "bioenergy: agricultural waste (syngas)": "syngas",
+    "bioenergy: agricultural waste (unknown)": "biomass",
+    "bioenergy: biodiesel": "diesel",
+    "bioenergy: ethanol": "ethanol",
+    "bioenergy: refuse (syngas)": "syngas",
+    "bioenergy: wastewater and sewage sludge (solids or biogas)": "biomass",
+    "bioenergy: wood & other biomass (biocoal)": "biomass",
+    "bioenergy: wood & other biomass (syngas)": "syngas",
+    "other: tires": "solid waste",
+    "hydrogen": "hydrogen",
+}
 FUEL_PATTERNS = {
     key: (
         re.compile(rf"\b{re.escape(key)}\b", flags=re.IGNORECASE)
@@ -58,10 +74,9 @@ FUEL_PATTERNS = {
     )
     for key in FUEL_MAPPING
 }
-GEM_CRS = "EPSG:4326"
 
 
-def _get_fuel(cell: str, default: str):
+def _fuel(cell: str, default: str):
     """Find and replace fuel names using pattern matching.
 
     Ambiguous cases should raise errors.
@@ -87,14 +102,14 @@ def _get_fuel(cell: str, default: str):
     return fuels
 
 
-def _get_powerplant_id(gem_df: pd.DataFrame) -> pd.Series:
+def _powerplant_id(gem_df: pd.DataFrame) -> pd.Series:
     """Create a unique identifier using GEM codes."""
     return gem_df.apply(
         lambda x: f"GEM_{x['GEM location ID']}_{x['GEM unit/phase ID']}", axis="columns"
     )
 
 
-def _get_name(gem_df: pd.DataFrame) -> pd.Series:
+def _name(gem_df: pd.DataFrame) -> pd.Series:
     """Create a unique name using GEM data."""
     return gem_df.apply(
         lambda x: f"{x['Plant / Project name']}_{x['Unit / Phase name']}",
@@ -102,12 +117,7 @@ def _get_name(gem_df: pd.DataFrame) -> pd.Series:
     )
 
 
-def _get_geometry(gem_df: pd.DataFrame, crs: str) -> gpd.GeoSeries:
-    """Converts lat/long to point data."""
-    return gpd.points_from_xy(gem_df["Longitude"], gem_df["Latitude"], crs=crs)
-
-
-def _get_technology(gem_df: pd.DataFrame, mapping: dict[str, str]) -> pd.Series:
+def _technology(gem_df: pd.DataFrame, mapping: dict[str, str]) -> pd.Series:
     """Remap technology names, cleaning CCS specifics and inconsistencies."""
     return (
         gem_df["Technology"]
@@ -117,7 +127,7 @@ def _get_technology(gem_df: pd.DataFrame, mapping: dict[str, str]) -> pd.Series:
     )
 
 
-def _get_ccs(gem_df: pd.DataFrame) -> pd.Series:
+def _ccs(gem_df: pd.DataFrame) -> pd.Series:
     """Get CCS status of powerplants."""
     return (
         gem_df[["Technology", "CCS", "Fuel"]]
@@ -131,35 +141,50 @@ def _get_ccs(gem_df: pd.DataFrame) -> pd.Series:
     )
 
 
-def _get_year(gem_df: pd.DataFrame, option: Literal["start", "end"]):
+def _year(gem_df: pd.DataFrame, option: Literal["start", "end"]):
     """Get start/end year, ensuring typing is respected."""
     mapping = {"start": "Start year", "end": "Retired year"}
     return gem_df[mapping[option]].apply(lambda x: np.nan if x == "not found" else x)
 
 
-def _get_chp(gem_df: pd.DataFrame) -> pd.Series:
+def _chp(gem_df: pd.DataFrame) -> pd.Series:
     """Get CHP status of powerplants."""
     return gem_df["CHP"].fillna("").apply(lambda x: True if "yes" in x else False)
 
 
 def get_powerplant_df(
-    raw_df: pd.DataFrame, tech_mapping: dict[str, str]
+    gem_df: pd.DataFrame, tech_mapping: dict[str, str]
 ) -> gpd.GeoDataFrame:
     """Obtain a standardised dataset for non-combustion powerplants."""
     capacity_df = gpd.GeoDataFrame(
         {
-            "powerplant_id": _get_powerplant_id(raw_df),
-            "name": _get_name(raw_df),
-            "category": raw_df["Type"],
-            "technology": _get_technology(raw_df, tech_mapping),
-            "output_capacity_mw": raw_df["Capacity (MW)"],
-            "start_year": _get_year(raw_df, "start"),
-            "end_year": _get_year(raw_df, "end"),
-            "status": raw_df["Status"],
-            "geometry": _get_geometry(raw_df, GEM_CRS),
+            "powerplant_id": _powerplant_id(gem_df),
+            "name": _name(gem_df),
+            "category": gem_df["Type"],
+            "technology": _technology(gem_df, tech_mapping),
+            "output_capacity_mw": gem_df["Capacity (MW)"],
+            "start_year": _year(gem_df, "start"),
+            "end_year": _year(gem_df, "end"),
+            "status": gem_df["Status"],
+            "geometry": get_point(gem_df, "Longitude", "Latitude"),
         }
     )
     return _schemas.PlantSchema.validate(capacity_df)
+
+
+def get_gem_df(path: str, category: str) -> pd.DataFrame:
+    """Get a GEM dataset for a type/category of powerplant."""
+    gem_df = pd.read_excel(path, sheet_name="Power facilities")
+    # Get raw dataset without cancelled projects
+    pattern = "|".join(map(re.escape, INVALID_STATE_VALUES))
+    mask = gem_df["Status"].str.contains(pattern, na=False)
+    gem_df = gem_df[~mask]
+    # Remove rows with problematic empty values
+    gem_df = gem_df.dropna(subset=DROP_NA_COLUMNS)
+    # Type/category should match with our schema
+    gem_df["Type"] = gem_df["Type"].replace(CATEGORY_MAPPING)
+    gem_df = gem_df[gem_df["Type"] == category]
+    return gem_df
 
 
 def get_combustion_plant_df(
@@ -170,70 +195,48 @@ def get_combustion_plant_df(
     Includes CCS and CHP attributes.
     """
     capacity_df = get_powerplant_df(raw_df, tech_mapping)
-    capacity_df["ccs"] = _get_ccs(raw_df)
-    capacity_df["chp"] = _get_chp(raw_df)
+    capacity_df["ccs"] = _ccs(raw_df)
+    capacity_df["chp"] = _chp(raw_df)
     return _schemas.CombustionSchema.validate(capacity_df)
 
 
-def get_combustion_fuel_df(raw_df: pd.DataFrame, default: str):
-    """Get a 'long' dataframe with fuels used per powerplant."""
-    fuels_df = pd.DataFrame(
-        {
-            "powerplant_id": _get_powerplant_id(raw_df),
-            "fuel": raw_df["Fuel"].apply(_get_fuel, default=default),
-        }
-    )
-    fuels_df = fuels_df.explode("fuel").reset_index(drop=True)
-    return _schemas.FuelSchema.validate(fuels_df)
-
-
-def combustion_powerplants(
-    gem_raw: pd.DataFrame, params: SmkParams, outputs: SmkOutput
+def prepare_powerplants(
+    gem_raw_path: str,
+    output_plants_path: str,
+    category: str,
+    technology_mapping: dict[str, str],
+    output_fuels_path: str | None,
+    default_fuel: str | None,
 ):
-    """Obtain and save datasets for combustion powerplants.
+    """Process powerplants that burn fuel."""
+    gem_df = get_gem_df(gem_raw_path, category)
+    plants_df = get_powerplant_df(gem_df, technology_mapping)
 
-    These have a unique structure due to CHP and CCS options.
-    The separate fuel dataset ensures a 'tidy' structure for multi-fuel powerplants.
-    """
-    capacity_dfs = []
-    fuel_dfs = []
-    for category in ["bioenergy", "coal", "oil_gas"]:
-        raw = gem_raw[gem_raw["Type"] == category]
-        capacity_dfs.append(
-            get_combustion_plant_df(raw, params["technology_mapping"][category])
+    if category in COMBUSTION_CATEGORIES:
+        if output_fuels_path is None or default_fuel is None:
+            raise ValueError(f"Incomplete fuel definition for {category}.")
+        plants_df["ccs"] = _ccs(gem_df)
+        plants_df["chp"] = _chp(gem_df)
+        _schemas.CombustionSchema.validate(plants_df).to_parquet(output_plants_path)
+
+        fuels_df = pd.DataFrame(
+            {
+                "powerplant_id": _powerplant_id(gem_df),
+                "fuel": gem_df["Fuel"].apply(_fuel, default=default_fuel),
+            }
         )
-        fuel_dfs.append(get_combustion_fuel_df(raw, params["default_fuel"][category]))
-    combined_combustion_cap = _schemas.CombustionSchema.validate(
-        pd.concat(capacity_dfs)
-    )
-    combined_combustion_fuel = _schemas.FuelSchema.validate(pd.concat(fuel_dfs))
-    combined_combustion_cap.to_parquet(outputs["combustion_plants"])
-    combined_combustion_fuel.to_parquet(outputs["combustion_plant_fuels"])
-
-
-def main(inputs: SmkInput, outputs: SmkOutput, params: SmkParams):
-    """Process and save datasets for GEM powerplants.
-
-    These include fossil powerplants (coal, oil_gas), geothermal and nuclear.
-    """
-    # Get raw dataset without cancelled projects
-    gem_raw = pd.read_excel(inputs["gem_raw"], sheet_name="Power facilities")
-    pattern = "|".join(map(re.escape, ["cancelled", "shelved"]))
-    mask = gem_raw["Status"].str.contains(pattern, na=False)
-    gem_raw = gem_raw[~mask]
-    gem_raw["Type"] = gem_raw["Type"].replace("oil/gas", "oil_gas")
-
-    combustion_powerplants(gem_raw, params, outputs)
-    nuclear = get_powerplant_df(
-        gem_raw[gem_raw["Type"] == "nuclear"], params["technology_mapping"]["nuclear"]
-    )
-    nuclear.to_parquet(outputs["nuclear_plants"])
-    geothermal = get_powerplant_df(
-        gem_raw[gem_raw["Type"] == "geothermal"], params["technology_mapping"]["geothermal"]
-    )
-    geothermal.to_parquet(outputs["geothermal_plants"])
-
+        fuels_df = fuels_df.explode("fuel").reset_index(drop=True)
+        _schemas.FuelSchema.validate(fuels_df).to_parquet(output_fuels_path)
+    else:
+        _schemas.PlantSchema.validate(plants_df).to_parquet(output_plants_path)
 
 
 if __name__ == "__main__":
-    main(snakemake.input, snakemake.output, snakemake.params)
+    prepare_powerplants(
+        category=snakemake.wildcards.category,
+        gem_raw_path=snakemake.input.gem_raw,
+        technology_mapping=snakemake.params.technology_mapping,
+        default_fuel=snakemake.params.get("default_fuel", None),
+        output_plants_path=snakemake.output.powerplant_capacity,
+        output_fuels_path=snakemake.output.get("powerplant_fuels", None),
+    )
