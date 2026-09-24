e5df1055bc46f5c9166e3f72479e58756d03990d
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2026-09-24 19:29:21 +0200

    Use OpenStreetmap.DE for mapping

--- workflow/scripts/impute_time.py
+++ workflow/scripts/impute_time.py
@@ -12,6 +12,7 @@ import _utils
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import xyzservices.providers as xyz
 from _schemas import HISTORICAL, OPERATING, PLANNED, RETIRED, SCENARIO_MAP
 from _utils import DATASET_YEAR, EIA_CAT_MAPPING
 from matplotlib import pyplot as plt
@@ -970,8 +971,12 @@ def explore(
             file.write("No data")
     else:
         imputed.explore(
-            column="technology", legend=True, popup=True, cmap=colormap
-        ).save(output_path)
+        column="technology",
+        legend=True,
+        popup=True,
+        cmap=colormap,
+        tiles=xyz.OpenStreetMap.DE ,
+    ).save(output_path)
 
 
 def plot_powerplant_capacity_buildup(
